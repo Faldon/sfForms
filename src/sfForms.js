6f30e76667b7ba3ba9405c08b553ce85ade93ea1
Thomas Pulzer <t.pulzer@kniel.de>
2017-11-29 13:37:07 +0100

    Created a first functional prototype to drive tests with.

--- src/sfForms.js
+++ src/sfForms.js
@@ -8,68 +8,72 @@ var sfForms = (function () {
       allowDelete: true,
       addElementClass: [],
       deleteElementClass: [],
-      beforeAdd: function() {},
-      afterAdd: function() {},
-      beforeDelete: function() {},
-      afterDelete: function() {}
+      beforeAdd: function () {},
+      afterAdd: function () {},
+      beforeDelete: function () {},
+      afterDelete: function () {},
     },
-    sfForms = function(selector, options) {
-    return new init(selector, options)
-  };
+    sfForms = function (selector, options) {
+      return new init(selector, options)
+    },
+    _printMessage = function (message) { console.log(message) },
+    _dumpObject = function (object) { console.dir(object) },
+    _getNode = function (selector) {
+      var elements
+      if ((/^#.*/).test(selector)) {
+        return document.getElementById(selector.substr(1))
+      }
+      if ((/^\..*/).test(selector)) {
+        elements = document.getElementsByClassName(selector.substr(1))
+        if (elements.length > 0) {
+          return elements[0]
+        }
+      }
+      return null
+    },
+    printId = function () {
+      _printMessage(_collectionHolder)
+      return this
+    },
+    showOptions = function () {
+      _dumpObject(_options)
+      return this
+    },
+    init = function (selector, useroptions) {
+      _collectionHolder = _getNode(selector)
+      for (var p in useroptions) {
+        if (useroptions.hasOwnProperty(p)) _options[p] = useroptions[p]
+      }
+      Object.freeze(_options)
+      if (_collectionHolder === null) {
+        throw new EvalError('No suitable element found')
+      }
+      return this
+    }
 
-  _options['deleteTitel'] = "Delete this " + _options.elementType
-  _options['addTitel'] = "Add " + _options.elementType
-  _options['addElementLink'] = function() {
+  _options.deleteTitel = 'Delete this ' + _options.elementType
+  _options.addTitel = 'Add ' + _options.elementType
+  _options.addElementLink = function () {
     var link = '<button class="'
-    link += _options.addElementClass.join(" ")
-    link += '">' + _options.addTitel +'</button>'
+    link += _options.addElementClass.join(' ')
+    link += '">' + _options.addTitel + '</button>'
     return link
   }()
-  _options['deleteElementLink'] = function() {
+  _options.deleteElementLink = function () {
     var link = '<a href="#" class="'
-    link += _options.deleteElementClass.join(" ")
+    link += _options.deleteElementClass.join(' ')
     link += '" title="' + _options.deleteTitel + '"></a>'
     return link
   }()
 
-  var _printMessage = function (message) { console.log(message) }
-
-  var _dumpObject = function (object) { console.dir(object) }
-
-  var _getNode = function(selector) {
-    if((/^#.*/).test(selector))
-      return document.getElementById(selector.substr(1))
-    if((/^\..*/).test(selector)) {
-      var elements = document.getElementsByClassName(selector.substr(1))
-      if(elements.length > 0)
-        return elements[0]
-    }
-    return null;
-  }
-
-  var init = function(selector, useroptions) {
-    _collectionHolder = _getNode(selector)
-    for(var p in useroptions) {
-      if(useroptions.hasOwnProperty(p)) _options[p] = useroptions[p]
-    }
-    Object.freeze(_options);
-    if (_collectionHolder === null)
-      throw new EvalError("No suitable element found")
-    return this
-  }
-
-  var printId = function () {
-    _printMessage(_collectionHolder)
-    return this;
-  };
-
   init.prototype = sfForms.prototype = {
     sfForms: version,
     constructor: sfForms,
-    options: function(){return _options}(),
-    printId: printId
+    options: function () { return _options }(),
+    printId: printId,
+    showOptions: showOptions,
   }
 
-  return sfForms;
+  return sfForms
 
-})(sfForms || {});
+})(sfForms || {})
