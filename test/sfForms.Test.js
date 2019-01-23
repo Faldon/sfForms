9526dad68310df3d0477dccf164b3e828d36e98d
Thomas Pulzer <t.pulzer@kniel.de>
2019-01-23 10:35:39 +0100

    Deleted unneccessary project files from git.

--- test/sfForms.Test.js
+++
@@ -1,9 +0,0 @@
-var assert = require('assert');
-describe('SfForms', function() {
-  describe('#printId()', function() {
-    it('should print "id"', function() {
-      var sfForms = new sfForms('id');
-      assert.equal('id', sfForms.printId());
-    });
-  });
-});
